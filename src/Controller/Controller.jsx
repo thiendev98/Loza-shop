a5732e72869365e03bb5f3bbb11dbfd21e5e89b0
hoangthien <thien.dohoangcs98@gmail.com>
2022-05-05 11:04:22 +0700

    update footer

--- src/Controller/Controller.jsx
+++ src/Controller/Controller.jsx
@@ -5,8 +5,10 @@ import Shirt from "../View/components/Shirt";
 import Skirt from "../View/components/Skirt";
 import Vest from "../View/components/Vest";
 import Trousers from "../View/components/Trousers";
+import { FaFacebook, FaTiktok, FaInstagram, FaTwitter } from "react-icons/fa";
 import "./styles/style.css";
 import logo from "./images/logo.png";
+import footerImg from "./images/footer.png";
 const PAGE_HOME = "home";
 const PAGE_VEST = "vest";
 const PAGE_SHIRT = "shirt";
@@ -16,37 +18,102 @@ const PAGE_TROUSERS = "trousers";
 export default function Controller() {
   const [page, setPage] = useState(PAGE_HOME);
   const nextPage = (pages) => setPage(pages);
+  const aboutLoza = [
+    {
+      title: "aboutLoza",
+      content: "VỀ LOZA",
+    },
+    {
+      title: "Introduce",
+      content: "Giới thiệu",
+    },
+    {
+      title: "sales",
+      content: "Tin khuyến mãi",
+    },
+    {
+      title: "notification",
+      content: "Thông báo",
+    },
+  ];
+  const supportCustomer = [
+    {
+      title: "support",
+      content: "HỖ TRỢ KHÁCH HÀNG",
+    },
+    {
+      title: "size",
+      content: "Hướng dẫn chọn size",
+    },
+    {
+      title: "returnPolicy",
+      content: "Chính sách đổi trả",
+    },
+    {
+      title: "delivery",
+      content: "Thanh toán và giao nhận",
+    },
+    {
+      title: "security",
+      content: "Chính sách bảo mật",
+    },
+    {
+      title: "terms",
+      content: "Điều khoản website",
+    },
+  ];
+  const infoContact = [
+    {
+      logo: "https://bizweb.dktcdn.net/100/438/408/themes/848101/assets/map.svg?1645868209648",
+      info: "Khu TMDV Trung Văn 1 - Nam Từ Liêm - Hà Nội ",
+    },
+    {
+      logo: "https://bizweb.dktcdn.net/100/438/408/themes/848101/assets/phone.svg?1645868209648",
+      info: "Hotline: 0981.475.666 ",
+    },
+    {
+      logo: "https://bizweb.dktcdn.net/100/438/408/themes/848101/assets/email.svg?1645868209648",
+      info: "sale@loza.vn",
+    },
+  ];
+
   return (
     <div id="Controller">
       <div id="header">
         <div className="navbar row container-fluid">
           <div
-            className="navbar--logo col-xl-1"
+            className="navbar__logo col-xl-1"
             onClick={() => setPage(PAGE_HOME)}
           >
-            <img className="logo__img" src={logo} />
+            <img className="logo--img" src={logo} />
           </div>
-          <ul className="navbar--list col-xl-5">
-            <li className="navbar--link" onClick={() => nextPage(PAGE_TSHIRT)}>
+          <ul className="navbar__list col-xl-5">
+            <li className="navbar__link" onClick={() => nextPage(PAGE_TSHIRT)}>
               Áo phông
             </li>
-            <li className="navbar--link" onClick={() => nextPage(PAGE_SHIRT)}>
+            <li className="navbar__link" onClick={() => nextPage(PAGE_SHIRT)}>
               Áo sơ mi
             </li>
             <li
-              className="navbar--link"
+              className="navbar__link"
               onClick={() => nextPage(PAGE_TROUSERS)}
             >
               Quần
             </li>
-            <li className="navbar--link" onClick={() => nextPage(PAGE_SKIRT)}>
+            <li className="navbar__link" onClick={() => nextPage(PAGE_SKIRT)}>
               Chân váy
             </li>
-            <li className="navbar--link" onClick={() => nextPage(PAGE_VEST)}>
+            <li className="navbar__link" onClick={() => nextPage(PAGE_VEST)}>
               Vest
             </li>
           </ul>
-          <div className="navbar--search col-xl-4">Search</div>
+          <div className="navbar__search col-xl-4">
+            <input
+              className="search__in"
+              type="text"
+              placeholder="Tìm sản phẩm"
+            />
+          </div>
           <ul className="navbar--user row container-fluid col-xl-2">
             <li className="user__user col-xl-4">Logo</li>
             <li className="user__heart col-xl-4">Logo</li>
@@ -64,7 +131,49 @@ export default function Controller() {
         {page === PAGE_TSHIRT && <TShirt />}
       </div>
       {/*  */}
-      <div id="footer">Footer</div>
+      <div id="footer">
+        <div className="footer row container-fluid">
+          <div className="col-xl-4 footer__logo">
+            <p>
+              “Website đang trong quá trình vận hành thử nghiệm. Quý khách gặp
+              sự cố cần phản ánh có thể gọi/zalo tới 0981.475.666 để được hỗ
+              trợ. Rất mong nhận được sự thông cảm. Trân trọng!”
+            </p>
+            <ul className="footer__list row container-fluid">
+              <li className="footer__link col-xl-1">
+                <FaFacebook className="icon" />
+              </li>
+              <li className="footer__link col-xl-1">
+                <FaInstagram className="icon" />
+              </li>
+              <li className="footer__link col-xl-1">
+                <FaTwitter className="icon" />
+              </li>
+              <li className="footer__link col-xl-1">
+                <FaTiktok className="icon" />
+              </li>
+            </ul>
+          </div>
+          <ul className="col-xl-2 footer__about">
+            {aboutLoza.map((about) => (
+              <li className="about--item">{about.content}</li>
+            ))}
+          </ul>
+          <ul className="col-xl-2 footer__support">
+            {supportCustomer.map((support) => (
+              <li className="support--item">{support.content}</li>
+            ))}
+          </ul>
+          <ul className="col-xl-4 footer__info">
+            {infoContact.map((info) => (
+              <li className="info--item">
+                <img className="info--item__img" src={info.logo} />
+                {info.info}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
